808be6cf26f4663737c6b35d8f59d8977cb07fd3
chimezie86 <onmezie@gmail.com>
2026-05-25 14:23:08 -0400

    Added greetMe.py

---
+++ greetMe.py
@@ -0,0 +1,13 @@
+# create a function that prints out any name that is passed to it
+def greet_me(name):
+    print("Hello "+name)
+
+# create a variable named author
+author = "Susan E. McGregor"
+
+# create another variable named editor
+editor = "Jeff Bleiel"
+
+# use my custom function, 'greet_me' to output "Hello" messages to each person
+greet_me(author)
+greet_me(editor)
