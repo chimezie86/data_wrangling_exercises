1a81353e9044555db58d4150dec8bb4e6f406881
chimezie86 <onmezie@gmail.com>
2026-05-25 14:14:30 -0400

    Added basicGreeting.py

---
+++ basicGreeting.py
@@ -0,0 +1,9 @@
+# create a variable named author 
+author = "Susan E. McGregor"
+
+# create another variable named editor
+editor = "Jeff Bleiel"
+
+# use the built-in print function to output "Hello" message to each person
+print("Hello "+author)
+print("Hello "+editor)
